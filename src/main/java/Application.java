a7aee4288a6b3bd4ef4c261fc40388be32310c37
Lavina <corporate19.lavina@gmail.com>
2021-10-21 13:42:30 +0530

    first commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+	private int b=90;
+    public static void main(String[] args) {
+//        int a =10;
+//        int b =20;'
+    	int a;
+        //System.out.println("addition "+a+b);
+        
+        Application app=null;
+        System.out.println(app.b) ;;
+        }
+}
